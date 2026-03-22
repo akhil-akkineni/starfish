f3488792a8f0802429fc76d07d230d58b9d5c7b7
akhil-akkineni <165851967+akhil-akkineni@users.noreply.github.com>
2026-03-21 21:38:09 -0400

    Update app.py

--- app.py
+++ app.py
@@ -94,4 +94,6 @@ def getSpotify():
             print("Not fetching...")            
         return(render_template("index.html"))
 #Runs the host site
-app.run()
+if __name__ == "__main__":
+    import os
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
